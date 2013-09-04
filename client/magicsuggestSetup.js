19f9d81de199293eef4ea9084bba56b62b4f5f2c
Jacob Cole <tmad4000@gmail.com>
2013-09-04 16:15:21 -0700

    attempted to add magicsuggest. not working yet

---
+++ client/magicsuggestSetup.js
@@ -0,0 +1,45 @@
+
+  $(function() {
+
+    alert();
+    msAutofillInline = $('.related-idea-add').magicSuggest({
+        // selectionPosition: 'right',
+        selectionCls: 'selectedx',
+        renderer: function(idea){
+            //console.log(idea.name)
+            return '<div>' +
+                    '<div style="font-family: Arial; font-weight: bold">' + idea.name + '</div>' +
+                    '<div><b>Text</b>: ' + idea.desc + '</div>' +
+                   '</div>';
+        },
+        minChars: 0,
+        typeDelay: 20,
+        selectionStacked: true,
+        method:'GET',
+        expanded:true,
+        expandOnFocus:true,
+        maxDropHeight:'500px',
+        name:'query',
+        data: [{
+            id: 0,
+            name: "Panda",
+            desc: "Pandas are great furry animals",
+            
+        },{
+            id: 1,
+            name: "Butterfly",
+            desc: "Butterflies fly better with theirs wings on",
+            
+        },{
+            id: 2,
+            name: "Dolphin",
+            desc: "Dolphins call themselves by name like we do",
+            
+        }],
+        //data: '/ajax/autocomplete/', //jsonData2
+        selectionPosition: 'right',
+        emptyText:'Add related ideas',
+        selectionStacked: true
+      });    
+
+  });
--- ideaflow-meteor-mongo.js
+++ ideaflow-meteor-mongo.js
@@ -2,6 +2,53 @@ Ideas = new Meteor.Collection('ideas');
 
 if (Meteor.isClient) {
 
+  $(function() {
+
+
+    window.msAutofillInline = $('.related-idea-add').magicSuggest({
+        // selectionPosition: 'right',
+        selectionCls: 'selectedx',
+        renderer: function(idea){
+            //console.log(idea.name)
+            return '<div>' +
+                    '<div style="font-family: Arial; font-weight: bold">' + idea.name + '</div>' +
+                    '<div><b>Text</b>: ' + idea.desc + '</div>' +
+                   '</div>';
+        },
+        minChars: 0,
+        typeDelay: 20,
+        selectionStacked: true,
+        method:'GET',
+        expanded:true,
+        expandOnFocus:true,
+        maxDropHeight:'500px',
+        name:'query',
+        data: [{
+            id: 0,
+            name: "Panda",
+            desc: "Pandas are great furry animals",
+            
+        },{
+            id: 1,
+            name: "Butterfly",
+            desc: "Butterflies fly better with theirs wings on",
+            
+        },{
+            id: 2,
+            name: "Dolphin",
+            desc: "Dolphins call themselves by name like we do",
+            
+        }],
+        //data: '/ajax/autocomplete/', //jsonData2
+        selectionPosition: 'right',
+        emptyText:'Add related ideas',
+        selectionStacked: true
+      });    
+
+  });
+
+
+
   Template.ideaList.helpers({
     idea: function() {
 //      return Ideas.find().fetch();
@@ -14,11 +61,11 @@ if (Meteor.isClient) {
     relatedIdea: function(id) {
       var relatedIdeas = [];
       var idea = Ideas.find(id).fetch()[0];
-      console.log('idea:', idea);
+
       for (var i = 0; i < idea.relatedIdeas.length; i++) {
         relatedIdeas.push( Ideas.find(idea.relatedIdeas[i]).fetch()[0] );
       }
-      console.log('related ideas:', relatedIdeas);
+      // sort here
       return relatedIdeas;
     }
   });
@@ -27,7 +74,6 @@ if (Meteor.isClient) {
 
   Template.ideaInput.events({
     'click .idea-submit' : function () {
-      console.log('clieckd');
       var title = $('.idea-title').val();
       var description = $('.idea-description').val();
       Ideas.insert({ title: title, description: description, relatedIdeas: [],timestamp:Date.now() });
@@ -36,10 +82,11 @@ if (Meteor.isClient) {
 
   Template.ideaList.events({
     'click .related-idea-submit': function(e) {
-      var title = $(e.target).prevAll('.related-idea-title:first').val();
+      var title = $(e.target).prevAll('.related-idea-add:first').val();
+      console.log('related idea title:', title);
       var mainIdeaId = $(e.target).closest('.main-idea').attr('id');
 
-      var relatedIdeaId = Ideas.insert({ title: title, description: '', relatedIdeas: [] });    
+      var relatedIdeaId = Ideas.insert({ title: title, description: '', relatedIdeas: [],timestamp:Date.now() });    
       Ideas.update(mainIdeaId, { $push: {relatedIdeas: relatedIdeaId} })
 
     }
@@ -51,3 +98,4 @@ if (Meteor.isServer) {
 
   });
 }
+        
