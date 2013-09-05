d12277e8f718e84628d3280915748885a56b7d26
Ankit Ranjan <me@ankit.io>
2013-09-04 19:02:03 -0700

    WE GOT THAT MAGIC SUGGEST THING WORKING

--- ideaflow-meteor-mongo.js
+++ ideaflow-meteor-mongo.js
@@ -2,50 +2,7 @@ Ideas = new Meteor.Collection('ideas');
 
 if (Meteor.isClient) {
 
-  $(function() {
-
-
-    window.msAutofillInline = $('.related-idea-add').magicSuggest({
-        // selectionPosition: 'right',
-        selectionCls: 'selectedx',
-        renderer: function(idea){
-            //console.log(idea.name)
-            return '<div>' +
-                    '<div style="font-family: Arial; font-weight: bold">' + idea.name + '</div>' +
-                    '<div><b>Text</b>: ' + idea.desc + '</div>' +
-                   '</div>';
-        },
-        minChars: 0,
-        typeDelay: 20,
-        selectionStacked: true,
-        method:'GET',
-        expanded:true,
-        expandOnFocus:true,
-        maxDropHeight:'500px',
-        name:'query',
-        data: [{
-            id: 0,
-            name: "Panda",
-            desc: "Pandas are great furry animals",
-            
-        },{
-            id: 1,
-            name: "Butterfly",
-            desc: "Butterflies fly better with theirs wings on",
-            
-        },{
-            id: 2,
-            name: "Dolphin",
-            desc: "Dolphins call themselves by name like we do",
-            
-        }],
-        //data: '/ajax/autocomplete/', //jsonData2
-        selectionPosition: 'right',
-        emptyText:'Add related ideas',
-        selectionStacked: true
-      });    
-
-  });
+  
 
 
 
@@ -91,6 +48,48 @@ if (Meteor.isClient) {
 
     }
   });
+
+  Template.ideaList.rendered = function() {
+     window.msAutofillInline = $('.related-idea-add').magicSuggest({
+          // selectionPosition: 'right',
+          selectionCls: 'selectedx',
+          renderer: function(idea){
+              //console.log(idea.name)
+              return '<div>' +
+                      '<div style="font-family: Arial; font-weight: bold">' + idea.name + '</div>' +
+                      '<div><b>Text</b>: ' + idea.desc + '</div>' +
+                     '</div>';
+          },
+          minChars: 0,
+          typeDelay: 20,
+          selectionStacked: true,
+          method:'GET',
+          expanded:true,
+          expandOnFocus:true,
+          maxDropHeight:'500px',
+          name:'query',
+          data: [{
+              id: 0,
+              name: "Panda",
+              desc: "Pandas are great furry animals",
+              
+          },{
+              id: 1,
+              name: "Butterfly",
+              desc: "Butterflies fly better with theirs wings on",
+              
+          },{
+              id: 2,
+              name: "Dolphin",
+              desc: "Dolphins call themselves by name like we do",
+              
+          }],
+          //data: '/ajax/autocomplete/', //jsonData2
+          selectionPosition: 'right',
+          emptyText:'Add related ideas',
+          selectionStacked: true
+        });
+  }
 }
 
 if (Meteor.isServer) {
