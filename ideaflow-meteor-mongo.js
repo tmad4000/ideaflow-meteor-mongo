04360d365ac0029d527c9f24943f348734bcac03
Jacob Cole <tmad4000@gmail.com>
2013-09-09 20:24:52 -0700

    fixed expansion partially

--- ideaflow-meteor-mongo.js
+++ ideaflow-meteor-mongo.js
@@ -3,102 +3,8 @@ Ideas = new Meteor.Collection('ideas');
 if (Meteor.isClient) {
 
 
-
-
-  Template.ideaList.helpers({
-    idea: function() {
-//      return Ideas.find().fetch();
-      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
-//TODO: reverse order
-
-//      return Ideas.find({}, {sort: {$natural: 1}}).fetch();
-//      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
-    },
-
-  });
-
-  Template.idea.helpers({
-    relatedIdea: function(id) {
-      var relatedIdeas = [];
-      var idea = Ideas.find(id).fetch()[0];
-
-      for (var i = 0; i < idea.relatedIdeas.length; i++) {
-        relatedIdeas.push( Ideas.find(idea.relatedIdeas[i]).fetch()[0] );
-      }
-      // sort here
-      return relatedIdeas;
-    }
-  })
-
-  Template.ideaNamesList.helpers({
-    idea: function() {
-//      return Ideas.find().fetch();
-      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
-//TODO: reverse order
-
-//      return Ideas.find({}, {sort: {$natural: 1}}).fetch();
-//      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
-    },
-   
-    
-  });
-
-
-
-  Template.ideaInput.events({
-    'click .idea-submit, keypress .' : function () {
-      var title = $('.idea-title').val();
-      var description = $('.idea-description').val();
-      Ideas.insert({ title: title, description: description, relatedIdeas: [],timestamp:Date.now() }); //#TODO dates server side
-    }
-  });
-
-
-  Template.idea.events({
-    'click .related-ideas >li': function(e) {
-      $target = $(e.target);
-      var newIdea = Ideas.find($target.data('id')).fetch()[0]
-      console.log(newIdea);
-      console.log($target.closest('.main-idea'));
-      console.log(Template.idea(newIdea));
-      $target.closest('.main-idea').children('.idea-list').append(Template.idea(newIdea));
-    }
-  })
-
-
-  Template.ideaList.events({
-    'click .related-idea-submit': function(e) {
-      /*var title = $(e.target).prevAll('.related-idea-add-ms:first input').val();
-      console.log('related idea title:', title);
-*/
-      
-      //console.log(x=$(e.target).nextAll('.related-idea-add-ms').first());
-      //console.log($(e.target))
-
-      var newRelIdeas = $(e.target).nextAll('.related-idea-add-ms').first().magicSuggest().getSelectedItems();
-     // console.log(x=newRelIdeas);
-
-      var mainIdeaId = $(e.target).closest('.main-idea').attr('id');
-
-      for(var i=0;i<newRelIdeas.length;i++) {
-        var relatedIdeaId = Ideas.insert({ 
-          title: newRelIdeas[i].name, 
-          description: ''+newRelIdeas[i].description, 
-          relatedIdeas: [],
-          timestamp: Date.now() 
-        });  //#TODO dates server side
-        Ideas.update(mainIdeaId, { $push: {relatedIdeas: relatedIdeaId} })
-      }
-
-    }
-  }
-
-
-
-  );
-
-  Template.ideaList.rendered = function() {
-     msAutofillInline = $('.related-idea-add').magicSuggest({
+  function autoSuggest() {
+         msAutofillInline = $('.related-idea-add').magicSuggest({
           // selectionPosition: 'right',
           cls: 'related-idea-add-ms',
           selectionCls: 'related-idea-selected-ms',
@@ -162,11 +68,110 @@ if (Meteor.isClient) {
         });
 
   }
+
+  Template.ideaList.helpers({
+    idea: function() {
+//      return Ideas.find().fetch();
+      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
+//TODO: reverse order
+
+//      return Ideas.find({}, {sort: {$natural: 1}}).fetch();
+//      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
+    },
+
+  });
+
+  Template.idea.helpers({
+    relatedIdea: function(id) {
+      var relatedIdeas = [];
+      var idea = Ideas.find(id).fetch()[0];
+
+      for (var i = 0; i < idea.relatedIdeas.length; i++) {
+        relatedIdeas.push( Ideas.find(idea.relatedIdeas[i]).fetch()[0] );
+      }
+      // sort here
+      return relatedIdeas;
+    }
+  })
+
+  Template.ideaNamesList.helpers({
+    idea: function() {
+//      return Ideas.find().fetch();
+      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
+//TODO: reverse order
+
+//      return Ideas.find({}, {sort: {$natural: 1}}).fetch();
+//      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
+    },
+   
+    
+  });
+
+
+
+  Template.ideaInput.events({
+    'click .idea-submit, keypress .' : function () {
+      var title = $('.idea-title').val();
+      var description = $('.idea-description').val();
+      Ideas.insert({ title: title, description: description, relatedIdeas: [],timestamp:Date.now() }); //#TODO dates server side
+    }
+  });
+
+
+  Template.idea.events({
+    'click .related-ideas >li': function(e) {
+      $target = $(e.target);
+      var newIdea = Ideas.find($target.data('id')).fetch()[0]
+      //alert();
+      //console.log(newIdea);
+      //console.log($target.closest('.main-idea'));
+      //console.log(Template.idea(newIdea));
+      $target.closest('.main-idea').children('.idea-list').append(Template.idea(newIdea));
+      autoSuggest();
+    }
+  })
+
+
+  Template.ideaList.events({
+    'click .related-idea-submit': function(e) {
+      /*var title = $(e.target).prevAll('.related-idea-add-ms:first input').val();
+      console.log('related idea title:', title);
+*/
+      
+      //console.log(x=$(e.target).nextAll('.related-idea-add-ms').first());
+      //console.log($(e.target))
+
+      var newRelIdeas = $(e.target).nextAll('.related-idea-add-ms').first().magicSuggest().getSelectedItems();
+     // console.log(x=newRelIdeas);
+
+      var mainIdeaId = $(e.target).closest('.main-idea').attr('id');
+
+      for(var i=0;i<newRelIdeas.length;i++) {
+        var relatedIdeaId = Ideas.insert({ 
+          title: newRelIdeas[i].name, 
+          description: ''+newRelIdeas[i].description, 
+          relatedIdeas: [],
+          timestamp: Date.now() 
+        });  //#TODO dates server side
+        Ideas.update(mainIdeaId, { $push: {relatedIdeas: relatedIdeaId} })
+      }
+
+    }
+  }
+
+
+
+  );
+
+  Template.idea.rendered = function() {
+    //console.log("aoeu")
+    autoSuggest();
+  }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
-  });
+  });''
 }
         
