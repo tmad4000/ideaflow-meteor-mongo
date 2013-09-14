7a0dd5c9e248de5d264e7a909b90a1b81c2b6f97
shadibarhoumi <shadibarhoumi@gmail.com>
2013-09-14 14:37:36 -0700

    expansion using session variables not really working

--- ideaflow-meteor-mongo.js
+++ ideaflow-meteor-mongo.js
@@ -34,13 +34,13 @@ if (Meteor.isClient) {
           $($(this).magicSuggest(getMagicSuggestOptions())).on('selectionchange', function(events, combo, selection) {
             console.log('selected item:', selection);
             var newRelIdeas = selection;
-            var mainIdeaId = combo.container.closest('.main-idea').attr('id');
+            var mainIdeaId = combo.container.closest('.main-idea').data('id');
 
             for (var i = 0; i < newRelIdeas.length; i++) {
                 var existingIdea = Ideas.find(newRelIdeas[i]._id).fetch()[0]; // see if related idea is already in db
                 var relatedIdeaId;
                 if (!existingIdea) { // if not already in db, insert the idea
-                  relatedIdeaId = Ideas.insert({ title: newRelIdeas[i].name,
+                  relatedIdeaId = Ideas.insert({ title: newRelIdeas[i].title,
                                                       description: '',
                                                       relatedIdeas: [],
                                                       timestamp:Date.now()
@@ -59,7 +59,10 @@ if (Meteor.isClient) {
   /////// IDEALIST TEMPLATE ////////
   Template.ideaList.helpers({
     idea: function() {
-      return Ideas.find({}, {sort: {timestamp: -1}}).fetch();
+      return Ideas.find({}, {sort: {timestamp: -1}}).fetch().map(function(e) {
+        e.expandedId = 'expanded-' + Math.floor(Math.random() * 10000000);
+        return e;
+      });
     }
   });
 
@@ -67,31 +70,57 @@ if (Meteor.isClient) {
     autoSuggest(); // attach magicsuggest to 
   };
 
+  /////// EXPANDED IDEA TEMPLATE ////////
+  Template.expandedIdea.helpers({
+    relatedIdea: function(id) {
+      var relatedIdeas = [];
+      var idea = Ideas.find(id).fetch()[0];
+      for (var i = 0; i < idea.relatedIdeas.length; i++) {
+        relatedIdeas.push( Ideas.find(idea.relatedIdeas[i]).fetch()[0] );
+      }
+      console.log('related idea for idea:', idea, ':', relatedIdeas);
+      return relatedIdeas;
+    }
+  });
+
   /////// IDEA TEMPLATE ////////
   Template.idea.helpers({
     relatedIdea: function(id) {
       var relatedIdeas = [];
       var idea = Ideas.find(id).fetch()[0];
-
       for (var i = 0; i < idea.relatedIdeas.length; i++) {
         relatedIdeas.push( Ideas.find(idea.relatedIdeas[i]).fetch()[0] );
       }
-      // sort here
       return relatedIdeas;
     },
-    expandedIdea: function(id) {
-      var expandedIdeas = Session.get('expandedIdeas');
-
+    expandedChild: function(id) {
+      var expandedChildren = [];
+      var expandedChildrenIds = Session.get('expanded-' + id) || [];
+      for (var i = 0; i < expandedChildrenIds.length; i++) {
+        expandedChildren.push(Ideas.find(expandedChildrenIds[i]).fetch()[0]);
+      }
+      return expandedChildren;
     }
   })
 
   Template.idea.events({
     'click .related-ideas > li': function(e) {
-      $target = $(e.target);
-      var newIdea = Ideas.find($target.data('id')).fetch()[0]
-      console.log(newIdea);
-      $target.closest('.main-idea').children('.idea-list').prepend(Template.idea(newIdea));
-      autoSuggest();
+      // $target = $(e.target);
+      // var newIdea = Ideas.find($target.data('id')).fetch()[0]
+      // console.log(newIdea);
+      // $target.closest('.main-idea').children('.idea-list').prepend(Template.idea(newIdea));
+      // autoSuggest();
+
+      var parentId = $(e.target).closest('.main-idea').data('id'); // id of main idea
+      var expandedIdea = Ideas.find($(e.target).data('id')).fetch()[0]; // current idea clicked on
+      var currentPath = Session.get('expanded-' + parentId);
+
+      if (currentPath) {
+        currentPath.push(expandedIdea._id); // add expanded id onto path list
+        Session.set('expanded-' + parentId, currentPath);
+      } else {
+        Session.set('expanded-' + parentId, [expandedIdea._id]);
+      }
     }
   });
   
